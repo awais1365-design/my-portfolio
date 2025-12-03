61e36ded7a28f20e3b44dfbfbd0ee0eaabc6d487
awais1365 <awais1365@gmail.com>
2025-12-03 12:06:48 +0500

    changes

--- src/components/Services/ServiceSection.tsx
+++ src/components/Services/ServiceSection.tsx
@@ -7,7 +7,8 @@ const ServiceSection = () => {
     <section id="services" className="my-14">
       <SectionHeading
         title="// Services / Offers:"
-        subtitle="I provide a comprehensive range of development services designed to deliver high-quality code and keep you ahead of the competition."
+        subtitle="I provide a comprehensive range of development services designed to deliver high-quality code and keep you ahead of the competition.
+        "
       />
 
       <div className="mt-8 grid grid-cols-1 gap-x-8 gap-y-8 md:mt-[3.75rem] md:grid-cols-3">
