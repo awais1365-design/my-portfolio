47a7143f8985f9846d8da375f3cb133b8e9b5fa6
awais1365 <awais1365@gmail.com>
2025-12-03 12:55:25 +0500

    changes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
-  output:"export",
+  output:"standalone",
 
   images: {
     remotePatterns: [
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 import { skillList } from '@/appData'
 import ContactSection from '@/components/Contact/ContactSection'
 import Hero from '@/components/Hero/Hero'
-import ProjectSection from '@/components/Projects/ProjectSection'
+// import ProjectSection from '@/components/Projects/ProjectSection'
 import ServiceSection from '@/components/Services/ServiceSection'
 import Skills from '@/components/Skills/Skills'
-import TestimonialSection from '@/components/Testimonials/TestimonialSection'
-import { getAllProjects, getAllTestimonials } from '@/services'
+// import TestimonialSection from '@/components/Testimonials/TestimonialSection'
+// import { getAllProjects, getAllTestimonials } from '@/services'
 
 export default async function Home() {
-  const projects = await getAllProjects()
-  const testimonials = await getAllTestimonials()
+  // const projects = await getAllProjects()
+  // const testimonials = await getAllTestimonials()
 
   return (
     <main>
