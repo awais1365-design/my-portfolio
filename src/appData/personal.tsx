97084c9cc152e8ad573beeab5dd68a41e8b7dfaf
awais1365 <awais1365@gmail.com>
2025-12-03 17:10:24 +0500

    updated

--- src/appData/personal.tsx
+++ src/appData/personal.tsx
@@ -3,8 +3,8 @@ import { Codepen, Facebook, GithubIcon, Instagram, LinkedIn, X } from '@/utils/i
 export const socials = [
   { href: 'https://github.com/awais1365-design', icon: <GithubIcon /> },
   { href: 'https://www.linkedin.com/in/muhammad-a-422524b7/', icon: <LinkedIn /> },
-  { href: '', icon: <Codepen /> },
-  { href: '', icon: <X /> },
-  { href: '', icon: <Instagram /> },
-  { href: '', icon: <Facebook /> },
+  // { href: '', icon: <Codepen /> },
+  // { href: '', icon: <X /> },
+  // { href: '', icon: <Instagram /> },
+  // { href: '', icon: <Facebook /> },
 ]
